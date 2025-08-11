59e61b0a65b7de15c20047db517e35bbe3f1cfc2
Katarzyna Babiak <katarzynababiak@MacBook-Air-Katarzyna.local>
2025-08-11 16:46:51 +0200

    Initial Spring Boot app

---
+++ src/main/java/pl/kasia/devops/DevopsPortfolioApplication.java
@@ -0,0 +1,12 @@
+package pl.kasia.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsPortfolioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsPortfolioApplication.class, args);
+    }
+}
+
